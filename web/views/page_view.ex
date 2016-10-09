bc3b52752162aed15ba673764d9f1bd87d1dff6a
Nate Taylor <taylonr@gmail.com>
2016-10-09 15:30:14 -0500

    Template to handle data structure.

--- web/views/page_view.ex
+++ web/views/page_view.ex
@@ -1,3 +1,9 @@
 defmodule CouchjitsuTrack.PageView do
   use CouchjitsuTrack.Web, :view
+
+  def events(%{events: events}), do: events
+  def events(_conn) do
+    [%{date: "October 9", events: [%{name: "Technique", time: "1 hr"}, %{name: "Sparring", time: "1.5 hrs"}]}, %{date: "October 10", events: [%{name: "Judo", time: "1 hr"}]}
+    ]
+  end
 end
