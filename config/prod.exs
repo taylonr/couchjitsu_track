0861df3d25ddddf030b3f81421502bd19e876d19
Nate Taylor <taylonr@gmail.com>
2016-10-14 20:51:16 -0500

    Updating config

--- config/prod.exs
+++ config/prod.exs
@@ -67,7 +67,7 @@ config :logger, level: :info
 # which should be versioned separately.
 #import_config "prod.secret.exs"
 
-config :hello_phoenix, CouchjitsuTrack.Repo,
+config :couchjitsu_track, CouchjitsuTrack.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
