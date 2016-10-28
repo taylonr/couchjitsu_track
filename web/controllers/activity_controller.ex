774be625bb12b8cfc534ab32dff3b2106bdf5e36
Nate Taylor <taylonr@gmail.com>
2016-10-27 20:34:52 -0500

    Adding nav items

--- web/controllers/activity_controller.ex
+++ web/controllers/activity_controller.ex
@@ -5,7 +5,7 @@ defmodule CouchjitsuTrack.ActivityController do
     def index(conn,  %{"id" => id}) do
         activities = CouchjitsuTrack.ActivityHistory.get_history_for_id(id)
         title = CouchjitsuTrack.ActivityHistory.get_name(id)
-
+        
         render conn, "index.html", activities: activities, title: title
     end
 end
