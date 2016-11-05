db2b28aa9be9f32f028fbca55977b173ed8b21f3
Nate Taylor <taylonr@gmail.com>
2016-11-05 16:42:29 -0500

    Redirect to the add page if user hits the main page and is already signed in.

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,6 +2,9 @@ defmodule CouchjitsuTrack.PageController do
   use CouchjitsuTrack.Web, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    case conn.cookies["user_id"] do
+      nil -> render conn, "index.html"
+      _ -> redirect conn, to: "/activityfeed/new"
+    end
   end
 end
