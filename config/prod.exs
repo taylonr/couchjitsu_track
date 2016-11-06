dc87d75759cb68b8004673dc02b26d0475e197ca
Nate Taylor <taylonr@gmail.com>
2016-11-06 17:39:47 -0600

    Dont force SSL

--- config/prod.exs
+++ config/prod.exs
@@ -14,7 +14,7 @@ use Mix.Config
 config :couchjitsu_track, CouchjitsuTrack.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: "devtrack.couchjitsu.com", port: 443],
-  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  # force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
