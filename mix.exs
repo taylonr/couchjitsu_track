f8fa643e024a46bb5ddd4ffcfe7d822664000368
Nate Taylor <taylonr@gmail.com>
2018-04-10 18:03:58 -0500

    Updating elixir version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule CouchjitsuTrack.Mixfile do
   def project do
     [app: :couchjitsu_track,
      version: "0.0.1",
-     elixir: "~> 1.0",
+     elixir: "~> 1.5",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
